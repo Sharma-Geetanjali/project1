e31b2165826482540a0c8ba08c4948238aeba34b
geet <geet.sgs@gmail.com>
2022-07-10 02:22:37 -0700

    first

---
+++ Desktop/Project/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Contact from './Contact';
+// import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Contact/>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;

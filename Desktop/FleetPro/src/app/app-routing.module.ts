d86ab419e7dac0fad2d9887ddb91a3f1fa8e6ebe
geet <geet.sgs@gmail.com>
2022-09-28 01:01:55 -0700

    up

--- Desktop/FleetPro/src/app/app-routing.module.ts
+++ Desktop/FleetPro/src/app/app-routing.module.ts
@@ -424,7 +424,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
